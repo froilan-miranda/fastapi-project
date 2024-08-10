a0e6120d3039e583e8ebc0fc67143400259fb0d1
vaquero <froilan@virtual-artifact.com>
2024-08-10 13:35:14 +0100

    chores: update email service

--- src/services/email_service.py
+++ src/services/email_service.py
@@ -5,11 +5,7 @@ from src.config import config
 
 logger = logging.getLogger(__name__)
 
-ses_client = boto3.client(
-    "ses",
-    region_name=config.AWS_REGION_NAME,
-    aws_access_key_id=config.AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY,
+ses_client = boto3.client(service_name="ses", region_name=config.AWS_REGION_NAME, aws_access_key_id=config.AWS_ACCESS_KEY_ID, aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY,
 )
 
 #async def send_email(subject: str, body: str, recipients: list[str]) -> None:
