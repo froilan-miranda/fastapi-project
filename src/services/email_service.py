cedcbc9f2029fb38d7cb64b3c3e6ae8e728f0e76
vaquero <froilan@virtual-artifact.com>
2024-08-10 13:59:45 +0100

    chores: move boto client instanciation into method

--- src/services/email_service.py
+++ src/services/email_service.py
@@ -5,17 +5,18 @@ from src.config import config
 
 logger = logging.getLogger(__name__)
 
-ses_client = boto3.client(
-    service_name="ses",
-    region_name=config.AWS_REGION_NAME,
-    aws_access_key_id=config.AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY,
-)
+def get_email_client():
+    return boto3.client(
+        service_name="ses",
+        region_name=config.AWS_REGION_NAME,
+        aws_access_key_id=config.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY,
+    )
 
 #async def send_email(subject: str, body: str, recipients: list[str]) -> None:
 async def send_email(to: str, subject: str, body: str) -> None:
     logger.info("Sending email")
-    response = ses_client.send_email(
+    response = get_email_client().send_email(
         Destination={
             'ToAddresses': [
                 to
